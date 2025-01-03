d378939948dd2d1a1850c7b7709a189d6401bdbd
tprabhakarreddy <105852590+tprabhakarreddy@users.noreply.github.com>
2025-01-03 13:43:49 -0800

    Delete Day-3-State directory

--- Day-3-State/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "prt-state-bucket"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
-}
--- Day-3-State/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "dev" {
-  ami           = "ami-07d9cf938edb0739b"
-  instance_type = "t2.nano"
-  key_name      = "newkey"
-  tags = {
-    Name = "LocalInstance"
-  }
-
-}
