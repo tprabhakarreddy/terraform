3eba8556a131bd9c995be9b0d6367506effee789
tprabhakarreddy <105852590+tprabhakarreddy@users.noreply.github.com>
2025-01-05 16:32:57 -0800

    Delete Day-4-S3-DynamoDB-Creation directory

--- Day-4-S3-DynamoDB-Creation/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "dev" {
-  ami           = "ami-07d9cf938edb0739b"
-  instance_type = "t2.micro"
-  key_name      = "newkey"
-  tags = {
-    Name = "LocalInstance"
-  }
-
-}
--- Day-4-S3-DynamoDB-Creation/output.tf
+++
@@ -1,5 +0,0 @@
-output "instance_public_ip" {
-  value       = aws_instance.dev.public_ip
-  description = "The public IP address of the created EC2 instance"
-}
-
