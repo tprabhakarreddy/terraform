eb6e594c48a82c6a9bb2d52bbb3460307cda145f
prabhakar reddy <t.prabhakarreddy@gmail.com>
2025-01-08 11:02:49 -0800

    Day-7

--- Day-7-Modules-terraformRegistry-ec2/main.tf
+++ Day-7-Modules-terraformRegistry-ec2/main.tf
@@ -1,5 +1,5 @@
 module "name" {
-  # source        = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+  source        = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
   ami           = "ami-07d9cf938edb0739b"
   instance_type = "t2.micro"
   key_name      = "newkey"
